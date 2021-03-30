37cfdae8cdd7cb0939d773970ea18e8beca3f76f
syzonehq <57961326+syzonehq@users.noreply.github.com>
2021-03-30 21:04:30 +0530

    Update UnilendFlashLoanReceiverBase.sol

--- contracts/UnilendFlashLoanReceiverBase.sol
+++ contracts/UnilendFlashLoanReceiverBase.sol
@@ -217,17 +217,31 @@ contract UnilendFlashLoanReceiverBase {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     
-    address payable public unilendCoreAddress;
+    mapping(uint => address payable) private coreAddress;
     address payable public unilendFlashLoanCore;
     
     constructor() {
-        unilendCoreAddress = payable(0xf09f8Fb3f0224b835f190CC801F5F1a2f9fAbf40);
+        coreAddress[1] = payable(0x13A145D215182924c89F2aBc7D358DCc72F8F788);
+        coreAddress[3] = payable(0x13A145D215182924c89F2aBc7D358DCc72F8F788);
     }
     
     receive() payable external {}
     
     function ethAddress() internal pure returns(address) {
         return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    }
+    
+    function getChainID() internal view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+    
+    function getUnilendCoreAddress() internal view returns(address payable) {
+        require(coreAddress[getChainID()] == address(0), "UnilendV1: Chain not supported");
+        return coreAddress[getChainID()];
     }
     
     function getBalanceInternal(address _target, address _reserve) internal view returns(uint256) {
@@ -247,7 +261,7 @@ contract UnilendFlashLoanReceiverBase {
     }
     
     function flashLoan(address _target, address _asset, uint256 _amount, bytes memory _params) internal {
-        IUnilendFlashLoanCoreBase(unilendCoreAddress).flashLoan(_target, _asset, _amount, _params);
+        IUnilendFlashLoanCoreBase(getUnilendCoreAddress()).flashLoan(_target, _asset, _amount, _params);
     }
     
 }
